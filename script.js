9316f0c1efb3ced72dec36587ade975440cf8b5f
pieceofwater <kriegg878@gmail.com>
2020-12-12 13:26:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById('mw-content-text').innerHTML = document.getElementById('mw-content-text').innerHTML.replace(/\[.*\]/g, '');
+
+//open wikipedia article and paste this code into your browser console
