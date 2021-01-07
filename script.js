85adf9f587c7ddaec450ae03fc983d9a21dec914
pieceofwater <kriegg878@gmail.com>
2021-01-07 19:54:46 +0300

    fix

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.getElementById('mw-content-text').innerHTML = document.getElementById('mw-content-text').innerHTML.replace(/\[.*\]/g, '');
+document.getElementById('mw-content-text').innerHTML = document.getElementById('mw-content-text').innerHTML.replace(/\[\d+\]/g, '');
 
 //open wikipedia article and paste this code into your browser console
